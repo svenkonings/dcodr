48c939774d753396805b6561f80c49841100ba30
meteoorkip <svenkonings@live.nl>
2021-01-14 21:48:16 +0100

    Add list of coders

--- src/lib/coders/Coders.ts
+++ src/lib/coders/Coders.ts
@@ -3,6 +3,10 @@ import {Caesar} from "./Caesar";
 
 const caesar = new Caesar();
 
+export const coders: string[] = [
+    caesar.name
+];
+
 export function getCoder(name: string): Coder | undefined {
     switch (name) {
         case caesar.name:
